811842e0113d233be612de6c3964fa4bea523663
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-12-02 13:00:29 +0200

    0x06-01

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for i in range(len(my_string)):
+        if my_string[i] == 'c':
+            print(my_string[:i]+""+my_string[i+1:])
+        elif my_string[i] == 'C':
+            print(my_string[:i]+""+my_string[i+1:])
+        else:
+            print(my_string)
+        return 0
