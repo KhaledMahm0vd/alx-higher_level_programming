f7cb7be07237f749fbb32465c3e8d3d2a0a602a0
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-12-02 13:10:07 +0200

    0x06-01

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in range(len(my_string)):
-        if my_string[i] == 'c':
-            print(my_string[:i]+""+my_string[i+1:])
-        elif my_string[i] == 'C':
+        if my_string[i] == 'C' or my_string[i] == 'c':
             print(my_string[:i]+""+my_string[i+1:])
         else:
             print(my_string)
