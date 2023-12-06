3e3e56a7fd5c86c36e55b5c7bb8162314ded8574
KhaledMahm0vd <12422020643438@pg.cu.edu.eg>
2023-12-06 20:53:51 +0200

    0x11-01

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        key_list = list(a_dictionary.keys())
+        score = 0
+        for i in key_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+        return score
